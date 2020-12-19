0bc52d43a8d3db3b3a2352f4fcd4a08330b6af21
Marko Milardic <mxm8813@g.rit.edu>
2020-12-19 20:41:18 +0100

    dependencies test

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import depthLimit from 'graphql-depth-limit';
 import { createServer } from 'http';
-import compression from 'compression';
-import cors from 'cors';
 import schema from './schema';
 
 const app = express();
@@ -12,8 +9,7 @@ const server = new ApolloServer({
   // validationRules: [depthLimit(7)],
 });
 
-app.use('*', cors());
-app.use(compression());
+
 server.applyMiddleware({ app, path: '/graphql' });
 
 const httpServer = createServer(app);
